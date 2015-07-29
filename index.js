ccee09eb09274e73770836a50ae11fa0f174cf70
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-29 13:39:30 +0200

    Added invokeAsync method

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * Library that makes it easier to invoke lambda functions with promises.
+ * 
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  29 Jul. 2015
+ */
+
+// module dependencies
+var AWS = require('aws-sdk'),
+    Q = require('q');
+
+// Create the lambda function
+var lambda = new AWS.Lambda();
+
+module.exports = {
+    /**
+     * Invokes another lambda function asynchronously. This means that the InvocationType is set
+     * to `Event` and it will return after the invocation succeeded.
+     * 
+     * @param  {string}     name        The name of the lambda function to invoke.
+     * @param  {object}     payload     The payload that should be send to the lambda function.
+     * @return {Promise}                The promise object.
+     */
+    invokeAsync: function(name, payload) {
+        return Q.promise(function(resolve, reject) {    
+            if(!name) {
+                // If the function is undefined, just resolve
+                throw new Error('Please provide a name')
+            }
+                
+            // Build up the message params
+            var params = {
+                FunctionName: name,
+                InvocationType: 'Event',
+                Payload: JSON.stringify(payload)
+            };
+            
+            // Invoke another lambda function
+            lambda.invoke(params, function(err, data) {
+                if(err) {
+                    // Reject if something went wrong
+                    return reject(err);
+                }
+                
+                // Resolve if everything went well
+                resolve();
+            })
+        });
+    }
+};
