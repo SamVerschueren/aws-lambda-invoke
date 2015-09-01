d967d8b44b26bf157f514b5e58f7e06031e99d95
Sam Verschueren <sam.verschueren@gmail.com>
2015-09-01 16:55:14 +0200

    The sync invoke method is now returning the data that is returned by the lambda function.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 /**
  * Library that makes it easier to invoke lambda functions with promises.
- * 
+ *
  * @author Sam Verschueren      <sam.verschueren@gmail.com>
  * @since  29 Jul. 2015
  */
@@ -11,88 +11,88 @@
 var Q = require('q');
 
 (function() {
-   
+
     /**
      * Initializes the library by creating a new AWS.Lambda object.
-     * 
+     *
      * @param {AWS}  AWS     The AWS object.
      */
     module.exports = function(AWS) {
         // Constructs a new lambda function
         module.exports.raw = new AWS.Lambda();
-        
+
         // Return the exports object
         return module.exports;
     };
-   
+
     /**
      * Invokes another lambda function synchronously. This means that the InvocationType is set
      * to `RequestResponse` and it will return after the called lambda function returns the result.
-     * 
+     *
      * @param  {string}     name        The name of the lambda function to invoke.
      * @param  {object}     payload     The payload that should be send to the lambda function.
      * @return {Promise}                The promise object.
      */
     module.exports.invoke = function(name, payload) {
         var lambda = this.raw;
-       
-        return Q.promise(function(resolve, reject) {    
+
+        return Q.promise(function(resolve, reject) {
             if(!name) {
                 // If the function is undefined, just resolve
                 throw new Error('Please provide a name')
             }
-                
+
             // Build up the message params
             var params = {
                 FunctionName: name,
                 InvocationType: 'RequestResponse',
                 Payload: JSON.stringify(payload)
             };
-            
+
             // Invoke another lambda function
             lambda.invoke(params, function(err, data) {
                 if(err) {
                     // Reject if something went wrong
                     return reject(err);
                 }
-                
+
                 // Resolve if everything went well
-                resolve();
+                resolve(data);
             })
         });
     };
-    
+
     /**
      * Invokes another lambda function asynchronously. This means that the InvocationType is set
      * to `Event` and it will return after the invocation succeeded.
-     * 
+     *
      * @param  {string}     name        The name of the lambda function to invoke.
      * @param  {object}     payload     The payload that should be send to the lambda function.
      * @return {Promise}                The promise object.
      */
     module.exports.invokeAsync = function(name, payload) {
         var lambda = this.raw;
-        
-        return Q.promise(function(resolve, reject) {    
+
+        return Q.promise(function(resolve, reject) {
             if(!name) {
                 // If the function is undefined, just resolve
                 throw new Error('Please provide a name')
             }
-                
+
             // Build up the message params
             var params = {
                 FunctionName: name,
                 InvocationType: 'Event',
                 Payload: JSON.stringify(payload)
             };
-            
+
             // Invoke another lambda function
             lambda.invoke(params, function(err, data) {
                 if(err) {
                     // Reject if something went wrong
                     return reject(err);
                 }
-                
+
                 // Resolve if everything went well
                 resolve();
             })
